7b5624723b96f5b61c228669362142e3ca5cdf1c
Max <max.sabate@gmail.com>
2022-01-25 15:17:35 -0800

    changed arrow to ::after. Added JS toggle

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('yea')
+const content = document.querySelector(".content");
+
+//checks if the target that was clicked is valid
+function validTarget(e) {
+    if(e.target.classList.contains("question")){
+        console.log('yep')
+        return true
+    }
+}
+
+//toggles the active class on selected div
+function active(e) {
+    if(validTarget(e)) {      
+        console.log('yes')
+        e.target.classList.toggle('active')
+    }
+}
+
+content.addEventListener('click', active)
